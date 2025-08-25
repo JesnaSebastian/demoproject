ed25cf743a88deda044fceeec1927593bbebd08c
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-08-25 07:43:02 +0530

    Added assertion

--- src/test/java/testcases/TC_01_shopPlanLink.java
+++ src/test/java/testcases/TC_01_shopPlanLink.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageobjects.shopplanobject;
 import testbase.baseclass;
@@ -10,6 +11,8 @@ public class TC_01_shopPlanLink extends baseclass {
     public void TC_01_selectplnbutton() throws InterruptedException {
         shopplanobject spb = new shopplanobject(driver);
         spb.click_shopplanlink();
+        String nxtpage_validation=spb.gettxt_selectaplanpage();
+        Assert.assertEquals(nxtpage_validation,"Select a plan for You","invalid page");
 
     }
 }
