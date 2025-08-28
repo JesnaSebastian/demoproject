63cd328aaa901673884647d4b0e533fa0e4cc0c2
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-08-28 20:49:14 +0530

    my cart _bring ypur own phone_Tc_03

---
+++ src/test/java/testcases/TC_03_bringYourPhone.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageobjects.mycart_bringyourphoneObject;
+import pageobjects.selectaplanobject;
+import pageobjects.shopplanobject;
+import testbase.baseclass;
+
+public class TC_03_bringYourPhone extends baseclass {
+    @Test
+    public void TC_03_bringurphone() throws InterruptedException {
+        shopplanobject shopPage = new shopplanobject(driver);
+        shopPage.click_shopplanlink();
+        Thread.sleep(5000);
+
+        selectaplanobject sap=new selectaplanobject(driver);
+        sap.click_selectaplan();
+        Thread.sleep(5000);
+
+        mycart_bringyourphoneObject byp=new mycart_bringyourphoneObject(driver);
+        byp.click_bringyourphone();
+        Thread.sleep(5000);
+
+        String simtypepagevalidation = byp.simtypepagevalidation();
+        Assert.assertEquals(simtypepagevalidation,"Select SIM type","failed to navigate navigate to SIM type page");
+
+
+    }
+}
